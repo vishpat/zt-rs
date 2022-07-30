2f8b207b1f21d093e4523260d8e8fa8fb16affc3
Vishal Patil <vishpat@gmail.com>
2022-07-30 15:53:29 -0400

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use ldap3::{LdapConn, Scope, SearchEntry};
-use ldap3::result::Result;
+use std::io::Read;
 
-fn main() -> Result<()> {
+use ldap3::{LdapConn, Scope, SearchEntry};
+use openssl::x509::X509 as X509Cert;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> { 
     let mut ldap = LdapConn::new("ldap://localhost:389")?;
     ldap.simple_bind("uid=jwt,ou=service,dc=openmicroscopy,dc=org", "secret")?;
 
@@ -11,8 +13,18 @@ fn main() -> Result<()> {
         "(uid=alice)",
         vec!["userCertificate"],
     )?.success()?;
-    for entry in rs {
-        println!("{:?}", SearchEntry::construct(entry));
+
+    if rs.len() == 0 {
+        println!("No user found");
+        return Ok(());
     }
+    
+    let entry = rs.into_iter().next().unwrap();
+    let search_entry = SearchEntry::construct(entry);
+    let dn = search_entry.dn;
+    let bin_atts = search_entry.bin_attrs;
+    let der_cert = bin_atts.get("userCertificate").unwrap()[0].clone();
+    let cert = X509Cert::from_der(&der_cert)?;
+    println!("{:?}", cert);
     Ok(ldap.unbind()?)
 }
