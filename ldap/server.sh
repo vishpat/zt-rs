d8c7a425e76daea8bedab2e90340f4368cf02599
Vishal Patil <vishpat@gmail.com>
2022-07-30 08:58:10 -0400

    Able to create ldap config

--- ldap/server.sh
+++ ldap/server.sh
@@ -5,4 +5,4 @@ set -x
 PWD=`pwd`
 sudo docker stop ldap
 sudo docker rm ldap
-sudo docker run --name ldap -d -p 389:10389 -e APACHEDS_INSTANCE=ztrs -v $PWD/config.ldif:/bootstrap/conf/config.ldif:ro openmicroscopy/apacheds
+sudo docker run --name ldap -d -p 389:10389 openmicroscopy/apacheds
