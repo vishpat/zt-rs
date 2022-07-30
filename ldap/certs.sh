8f70a1e5816da6a4aa8330fa534c9783cebf080d
Vishal Patil <vishpat@gmail.com>
2022-07-30 13:11:09 -0400

    Checkpoint

---
+++ ldap/certs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -f *.pem
+rm -f *.der
+
+users=("alice" "bob" "charlie" "dan")
+for usr in ${users[@]}; do
+  echo $usr
+  openssl genrsa -out private-key-$usr.pem 2048
+  openssl rsa -in private-key-$usr.pem -pubout -out public-key-$usr.pem
+  openssl req -new -x509 -key private-key-$usr.pem -out cert-$usr.pem -days 360
+  openssl x509 -outform DER -in cert-$usr.pem  -out $usr.der
+done
+
+ldapmodify -H ldap://localhost:389 -D uid=admin,ou=system -w secret cert.ldif
--- ldap/server.sh
+++ ldap/server.sh
@@ -6,3 +6,6 @@ PWD=`pwd`
 sudo docker stop ldap
 sudo docker rm ldap
 sudo docker run --name ldap -d -p 389:10389 openmicroscopy/apacheds
+
+sleep 10
+ldapadd -H ldap://localhost:389 -D uid=admin,ou=system -w secret -f config.ldif
