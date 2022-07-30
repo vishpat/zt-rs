5ed10444fc86debae0577db42d2817bf2ff9909c
Vishal Patil <vishpat@gmail.com>
2022-07-30 18:23:41 -0400

    Fixed clippy issues

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use ldap3::{LdapConn, Scope, SearchEntry};
-use openssl::x509::X509 as X509Cert;
-use openssl::rsa::{Rsa, Padding};
 use openssl::base64;
+use openssl::rsa::Padding;
+use openssl::x509::X509 as X509Cert;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> { 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut ldap = LdapConn::new("ldap://localhost:389")?;
     ldap.simple_bind("uid=jwt,ou=service,dc=openmicroscopy,dc=org", "secret")?;
 
-    let (rs, _res) = ldap.search(
-        "dc=openmicroscopy,dc=org",
-        Scope::Subtree,
-        "(uid=alice)",
-        vec!["userCertificate"],
-    )?.success()?;
+    let (rs, _res) = ldap
+        .search(
+            "dc=openmicroscopy,dc=org",
+            Scope::Subtree,
+            "(uid=alice)",
+            vec!["userCertificate"],
+        )?
+        .success()?;
 
-    if rs.len() == 0 {
+    if rs.is_empty() {
         println!("No user found");
         return Ok(());
     }
-    
+
     let entry = rs.into_iter().next().unwrap();
     let search_entry = SearchEntry::construct(entry);
     let bin_atts = search_entry.bin_attrs;
