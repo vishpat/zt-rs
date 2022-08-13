b7359490426b948f027d3bc3258cbc94c471ac6e
Vishal Patil <vishpat@gmail.com>
2022-08-13 12:13:30 -0400

    Checkpoint

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+
+provider "vault" {
+}
+
+# Enable Auditing
+
+resource "vault_audit" "test" {
+  type = "file"
+
+  options = {
+    file_path = "/tmp/audit.txt"
+  }
+}
+
+# Enable version 1 kv store
+
+resource "vault_mount" "kv" {
+  path        = "kv"
+  type        = "kv-v1"
+  description = "Key-Value store"
+}
+
+
+# Enable JWT authentication 
+
+resource "vault_jwt_auth_backend" "jwt_auth" {
+    description = "Enable JWT auth"
+    path = "jwt"
+    jwks_url = "http://localhost:8080/auth/jwks"
+}
+
+# Role for Alice to access key private secrets
+
+resource "vault_jwt_auth_backend_role" "jwt_alice" {
+  backend        = vault_jwt_auth_backend.jwt_auth.path
+  role_name      = "jwt_alice"
+  token_policies = ["alice_keys_policy"]
+
+
+  bound_subject   = "alice"
+  user_claim      = "sub"
+  role_type       = "jwt"
+}
+
+resource "vault_policy" "alice_keys_policy" {
+  name   = "alice_keys_policy"
+  policy = <<EOT
+
+path "secret/kv/users/alice/*" {
+    capabilities = [ "create", "read", 
+        "update", "delete", "list" ]
+}
+
+                EOT
+}
+
+# Role to access the eng secrets
+# 
+resource "vault_jwt_auth_backend_role" "jwt_eng" {
+  backend        = vault_jwt_auth_backend.jwt_auth.path
+  role_name      = "jwt_alice"
+  token_policies = ["eng_keys_policy"]
+
+  groups_claim  = ["eng"]
+  user_claim      = "sub"
+  role_type       = "jwt"
+}
+
+resource "vault_policy" "alice_keys_policy" {
+  name   = "alice_keys_policy"
+  policy = <<EOT
+
+path "secret/kv/groups/eng/*" {
+    capabilities = [ "create", "read", 
+      "update", "delete", "list" ]
+}
+
+                EOT
+}
